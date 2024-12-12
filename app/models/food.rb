6c7e36a0e101f200dcb4060a7e510d6ccbaab915
uw819kt <miya0arima@gmail.com>
2024-12-12 16:48:03 +0900

    correction_3

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :shop#ok
   has_many :order_foods
-  has_many :orders, turough: :order_foods
+  has_many :orders, through: :order_foods
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :address#ok
   belongs_to :customer#ok
   has_many :foods
-  has_many :foods, turough: :order_foods
+  has_many :foods, through: :order_foods
 end
